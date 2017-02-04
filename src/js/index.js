389a2f6fb20f2f719af466719c4ae6c889e0cbe2
PrzemyslawRaczkowski <przemyslaw.raczkowski@gmail.com>
2017-02-05 00:08:09 +0100

    Main component introduced

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MainComponent from './components/MainComponent'
+
+const app = document.getElementById('app');
+ReactDOM.render(<MainComponent/>, app);
