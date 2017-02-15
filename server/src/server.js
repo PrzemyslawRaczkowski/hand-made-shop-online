6d00a7b810b64dbf92eeaf990f7e852a6d8c211d
PrzemyslawRaczkowski <przemyslaw.raczkowski@gmail.com>
2017-02-15 23:00:46 +0100

    Code refactoring

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,34 @@
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
+const mongo = require('mongodb');
+const assert = require('assert');
+const url = 'mongodb://localhost:27017/handmade_shop';
 
-var app = express();
-var port = process.env.PORT || 5050;
-
+const router = express.Router();
+const app = express();
+const port = process.env.PORT || 5050;
+const products = require('./domain/products');
 
 app.use(express.static(path.join(process.cwd(), 'public')));
 
 app.get('/', function (request, response) {
     response.sendFile(path.join(process.cwd(), 'public', 'index.html'));
+});
+
+router.get('/shop', function (req, res, next) {
+    var loadedProducts = [];
+
+    mongo.connect(url, function (err, db) {
+        assert.equal(null, err);
+        var cursor = db.collection('products').find();
+        cursor.forEach(function (doc, err) {
+            assert.equal(null, err);
+            loadedProducts.push(doc);
+        }, function () {
+            db.close();
+            res.render(loadedProducts);
+        });
+    });
 });
 
 app.listen(port, err => {
