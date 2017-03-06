eb03573e4ee89357d89e8103f8040ec0c718b950
PrzemyslawRaczkowski <przemyslaw.raczkowski@gmail.com>
2017-03-06 22:19:06 +0100

    AddingProduct component added

--- client/src/js/pages/Faq.js
+++ client/src/js/pages/Faq.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-import MainComponent from '../components/MainComponent';
+import Header from '../components/Header';
+import Row from '../components/Row';
+import AddingProduct from '../components/AddingProduct';
 
 export default class Faq extends React.Component {
     render() {
         return (
             <div>
-                <MainComponent title="FAQ"/>
+                <Header/>
+                <Row title='Faq'/>
+                <AddingProduct/>
             </div>
         );
     }
--- server/src/routes/products.js
+++ server/src/routes/products.js
@@ -2,13 +2,10 @@ var ProductSchema = require('../schemas/productSchema');
 var ProductModel = mongoose.model('Product', ProductSchema);
 
 module.exports = function (app) {
-
     app.get('/shop', (request, response, next) => {
         ProductModel.find()
             .then(function (doc) {
                 response.json({productData: doc});
             });
     });
-
-
 };
